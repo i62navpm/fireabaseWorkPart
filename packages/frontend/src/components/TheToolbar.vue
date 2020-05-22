8048d81c629b45b726cdcdd13828d4cbe45804ee
Manuel Navarro Pérez <m.navarro@payvision.com>
2020-05-22 20:06:18 +0200

    chore: initial app shel model

---
+++ packages/frontend/src/components/TheToolbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar app elevate-on-scroll color="white">
+    <v-toolbar-title>Diario</v-toolbar-title>
+
+    <v-spacer />
+
+    <v-fade-transition leave-absolute></v-fade-transition>
+  </v-app-bar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.v-app-bar.v-app-bar--hide-shadow {
+  box-shadow: 0 1px 0 0 #dadce0;
+}
+</style>
