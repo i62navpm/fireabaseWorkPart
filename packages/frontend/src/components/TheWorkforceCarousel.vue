4452e13871a1ee6b544c6551c8ec3e286af4f99e
Manuel Navarro Pérez <m.navarro@payvision.com>
2020-05-26 19:51:55 +0200

    chore: set active worker

--- packages/frontend/src/components/TheWorkforceCarousel.vue
+++ packages/frontend/src/components/TheWorkforceCarousel.vue
@@ -27,56 +27,56 @@ export default {
   data: () => ({
     workforce: [
       {
-        id: 1,
+        id: '1',
         name: 'Valdemar Tellesson',
         description: 'Blister (nonthermal), right ankle',
       },
       {
-        id: 2,
+        id: '2',
         name: 'Alan Writtle',
         description: 'Traumatic cerebral edema w LOC of unsp duration, subs',
       },
       {
-        id: 3,
+        id: '3',
         name: 'Jervis Gehring',
         description: 'Laceration with foreign body of thigh',
       },
       {
-        id: 4,
+        id: '4',
         name: 'Gusti Valentim',
         description:
           'Anterior subluxation of left humerus, subsequent encounter',
       },
       {
-        id: 5,
+        id: '5',
         name: 'Emlyn Daglish',
         description: 'Disp fx of lunate, right wrist, init for clos fx',
       },
       {
-        id: 6,
+        id: '6',
         name: 'Rosalinde Long',
         description:
           'Unsp fracture of unsp forearm, init for opn fx type 3A/B/C',
       },
       {
-        id: 7,
+        id: '7',
         name: 'Pat Fairtlough',
         description:
           'Nondisp transverse fracture of shaft of right femur, init',
       },
       {
-        id: 8,
+        id: '8',
         name: 'Mada Thorsen',
         description: 'Enterostomy hemorrhage',
       },
       {
-        id: 9,
+        id: '9',
         name: 'Adham Endon',
         description:
           'Nondisp simple suprcndl fx w/o intrcndl fx unsp humer, sqla',
       },
       {
-        id: 10,
+        id: '10',
         name: 'Arri Ferencowicz',
         description: 'Dislocation of MTP joint of right lesser toe(s), init',
       },
