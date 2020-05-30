d7ec9c770af807b336a308294b074cb15c6963e7
Manuel Navarro Pérez <m.navarro@payvision.com>
2020-05-30 19:20:18 +0200

    chore: added vuexfire plugin

--- packages/frontend/src/store/modules/workforce.js
+++ packages/frontend/src/store/modules/workforce.js
@@ -1,12 +1,24 @@
+import { vuexfireMutations, firestoreAction } from 'vuexfire'
+import { db } from '@/plugins/firebase/db'
+
 export default {
   state: {
     workforce: [],
   },
   mutations: {
     setWorkforce: (state, workforce) => (state.workforce = workforce),
+    ...vuexfireMutations,
   },
   actions: {
-    setWorkforce: ({ commit }, workforce) => commit('setWorkforce', workforce),
+    workforceRef: firestoreAction(({ bindFirestoreRef, rootGetters }) =>
+      bindFirestoreRef(
+        'workforce',
+        db
+          .collection('users')
+          .doc(rootGetters.getUser.email)
+          .collection('workforces')
+      )
+    ),
   },
   getters: {
     getWorkforce: (state) => state.workforce,
--- packages/frontend/src/views/Workforce.vue
+++ packages/frontend/src/views/Workforce.vue
@@ -24,62 +24,7 @@ export default {
     TheWorkforceCarousel,
   },
   created() {
-    this.$store.dispatch('setWorkforce', [
-      {
-        id: '1',
-        name: 'Valdemar Tellesson',
-        description: 'Blister (nonthermal), right ankle',
-      },
-      {
-        id: '2',
-        name: 'Alan Writtle',
-        description: 'Traumatic cerebral edema w LOC of unsp duration, subs',
-      },
-      {
-        id: '3',
-        name: 'Jervis Gehring',
-        description: 'Laceration with foreign body of thigh',
-      },
-      {
-        id: '4',
-        name: 'Gusti Valentim',
-        description:
-          'Anterior subluxation of left humerus, subsequent encounter',
-      },
-      {
-        id: '5',
-        name: 'Emlyn Daglish',
-        description: 'Disp fx of lunate, right wrist, init for clos fx',
-      },
-      {
-        id: '6',
-        name: 'Rosalinde Long',
-        description:
-          'Unsp fracture of unsp forearm, init for opn fx type 3A/B/C',
-      },
-      {
-        id: '7',
-        name: 'Pat Fairtlough',
-        description:
-          'Nondisp transverse fracture of shaft of right femur, init',
-      },
-      {
-        id: '8',
-        name: 'Mada Thorsen',
-        description: 'Enterostomy hemorrhage',
-      },
-      {
-        id: '9',
-        name: 'Adham Endon',
-        description:
-          'Nondisp simple suprcndl fx w/o intrcndl fx unsp humer, sqla',
-      },
-      {
-        id: '10',
-        name: 'Arri Ferencowicz',
-        description: 'Dislocation of MTP joint of right lesser toe(s), init',
-      },
-    ])
+    this.$store.dispatch('workforceRef')
   },
 }
 </script>
