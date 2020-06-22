20e519a8f7af746a937f988194ccf7a6541e542b
Manuel Navarro Pérez <m.navarro@payvision.com>
2020-06-22 20:52:11 +0200

    chore: upload config files

---
+++ packages/frontend/src/config/firebase.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyAembfcyY0QLlgvVtFWXpbU3CmZit57dAg',
+  authDomain: 'work-part.firebaseapp.com',
+  databaseURL: 'https://work-part.firebaseio.com',
+  projectId: 'work-part',
+  storageBucket: 'work-part.appspot.com',
+  messagingSenderId: '333918670359',
+  appId: '1:333918670359:web:8bd4483081ef2198655652',
+  measurementId: 'G-5KV2JJ74JX',
+}
