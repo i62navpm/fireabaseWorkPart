36bce4f2e19d3968baa10a4477f67f0e7e8b3720
Manuel Navarro Pérez <m.navarro@payvision.com>
2020-06-22 23:13:59 +0200

    chore: change icons format type

--- packages/frontend/vue.config.js
+++ packages/frontend/vue.config.js
@@ -1,9 +1,43 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
   pwa: {
-    name: 'Nóminas',
-    themeColor: '#1976d2',
+    iconPaths: {
+      favicon32: null,
+      favicon16: null,
+      appleTouchIcon: 'img/icons/apple-touch-icon.webp',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.webp',
+    },
     manifestOptions: {
+      name: 'Nóminas',
+      short_name: 'Nóminas',
+      theme_color: '#1976d2',
+      icons: [
+        {
+          src: 'img/icons/android-chrome-192x192.webp',
+          sizes: '192x192',
+          type: 'image/webp',
+        },
+        {
+          src: 'img/icons/android-chrome-512x512.webp',
+          sizes: '512x512',
+          type: 'image/webp',
+        },
+        {
+          src: 'img/icons/android-chrome-maskable-192x192.webp',
+          sizes: '192x192',
+          type: 'image/webp',
+          purpose: 'maskable',
+        },
+        {
+          src: 'img/icons/android-chrome-maskable-512x512.webp',
+          sizes: '512x512',
+          type: 'image/webp',
+          purpose: 'maskable',
+        },
+      ],
+      start_url: '.',
+      display: 'standalone',
       background_color: '#ffffff',
     },
   },
